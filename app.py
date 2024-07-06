3aadaac67ca05f6efdb831ee5f9d7bab568fc464
amulyagowrij <amulyagowrij@gmail.com>
2024-07-06 20:36:13 +0100

    updated code

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from __future__ import division, print_function
-# coding=utf-8
 import sys
 import os
-import glob
-import re
 import numpy as np
-import tensorflow as tf
 import tensorflow as tf
 
 from tensorflow.compat.v1 import ConfigProto
@@ -15,51 +11,64 @@ config = ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.2
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
-# Keras
-from tensorflow.keras.applications.resnet50 import preprocess_input
+
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications import ResNet152V2
-from tensorflow.keras.applications.resnet_v2 import preprocess_input, decode_predictions
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
 
-# Flask utils
-from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
-
-
-# Define a flask app
 app = Flask(__name__)
 
+# Model saved with Keras model.save()
+MODEL_PATH ='model_resnet152V2.h5'
 
-# Load the model
-model = ResNet152V2(weights='imagenet')
+# Load your trained model
+model = load_model(MODEL_PATH)
 
-
-# Function to predict the image
 def model_predict(img_path, model):
+    print(img_path)
     img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image
     x = image.img_to_array(img)
+    x = x / 255.0  # Scale the image to [0, 1]
     x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
 
     preds = model.predict(x)
-    preds = decode_predictions(preds, top=1)[0]
+    preds = np.argmax(preds, axis=1)
+    
+    if preds == 0:
+        preds = "The leaf is diseased cotton leaf"
+    elif preds == 1:
+        preds = "The leaf is diseased cotton plant"
+    elif preds == 2:
+        preds = "The leaf is fresh cotton leaf"
+    else:
+        preds = "The leaf is fresh cotton plant"
+    
     return preds
 
-# Define routes
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
+    # Main page
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
     if request.method == 'POST':
+        # Get the file from post request
         f = request.files['file']
+
+        # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
-        result = model_predict(file_path, model)
-        return render_template('result.html', result=result)
-    return render_template('index.html')
 
+        # Make prediction
+        preds = model_predict(file_path, model)
+        result = preds
+        return result
+    return None
 
 if __name__ == '__main__':
-    app.run(port=5001,debug=True)
+    app.run(port=5001, debug=True)
